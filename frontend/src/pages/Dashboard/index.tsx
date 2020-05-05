ae84726b317d4953ff0bd2b5a1dbe263aa09cf0d
almerindo <almerindo.rehem@gmail.com>
2020-05-04 23:04:09 -0300

    feat: add axios and connect to api github

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,64 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
+interface Repository {
+  fullname: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Repositories on GitHub</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit"> Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/554495?s=460&u=ba5325b6341280d81ff751d7fde7f4a1bccd07f9&v=4"
-            alt="Almerindo Rehem"
-          />
-          <div>
-            <strong>sanar/errorhandler</strong>
-            <p>
-              Make simple and easy your error in Sentry! Semantic Sentry Error
-              Repot.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/554495?s=460&u=ba5325b6341280d81ff751d7fde7f4a1bccd07f9&v=4"
-            alt="Almerindo Rehem"
-          />
-          <div>
-            <strong>sanar/errorhandler</strong>
-            <p>
-              Make simple and easy your error in Sentry! Semantic Sentry Error
-              Repot.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/554495?s=460&u=ba5325b6341280d81ff751d7fde7f4a1bccd07f9&v=4"
-            alt="Almerindo Rehem"
-          />
-          <div>
-            <strong>sanar/errorhandler</strong>
-            <p>
-              Make simple and easy your error in Sentry! Semantic Sentry Error
-              Repot.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.fullname} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.fullname}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
